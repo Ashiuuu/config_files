d8a9894fbe1773b2a47901c3ae05f240c0b73669
Ashiu <clement.milisavljevic@epita.fr>
2021-09-14 18:00:53 +0200

    Fixes

--- zsh_install.sh
+++ zsh_install.sh
@@ -4,6 +4,6 @@ sudo apt update && sudo apt install -y zsh && chsh -s $(which zsh)
 mkdir .zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 cp .zshrc ~
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 zsh

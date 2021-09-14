94feadf1950378ddfcaf816609540a5723d6f3ca
Ashiu <clement.milisavljevic@epita.fr>
2021-09-14 17:18:33 +0200

    Modified install script to add zshrc installation

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt update && sudo apt install -y zsh && chsh -s $(which zsh)
+wget https://raw.githubusercontent.com/Ashiuuu/config_files/main/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git

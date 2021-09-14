3309d30670c11d7a9652ee9e5b5a73795c5cf3ac
Ashiu <clement.milisavljevic@epita.fr>
2021-09-14 17:41:12 +0200

    Modified installing procedure

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 sudo apt update && sudo apt install -y zsh && chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/Ashiuuu/config_files/main/.zshrc ~
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+cp -r .zsh ~
+cp .zshrc ~
+cp -r zsh-syntax-highlighting ~
+zsh

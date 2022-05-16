e0d7a342f96e0aef2ab28c4cc7d389fe54f3dbd4
Ashiuuu <ushiuako@gmail.com>
2022-05-16 20:17:23 +0200

    fixed typo in creating .zsh directory

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt update && sudo apt install -y zsh && chsh -s $(which zsh)
-mkdir .zsh
+mkdir ~/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 cp .zshrc ~
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting

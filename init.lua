31b0fd624b4fed0b9696eb048bc0c797ca00bc2b
Ashiuuu <ushiuako@gmail.com>
2025-08-29 10:58:23 +0200

    add xml support

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
 vim.o.wrap = false
 vim.o.tabstop = 4
-vim.o.shiftwidth = 4
 vim.o.swapfile = false
 vim.o.clipboard = "unnamedplus"
 
@@ -13,14 +12,14 @@ vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>pu', vim.pack.update)
 
 vim.pack.add({
-		"https://github.com/Mofiqul/vscode.nvim",
-		"https://github.com/neovim/nvim-lspconfig",
-		"https://github.com/stevearc/oil.nvim",
+	"https://github.com/Mofiqul/vscode.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/stevearc/oil.nvim",
 })
 
 require("oil").setup()
 
-vim.o.background = "light"
+vim.o.background = 'light'
 vim.cmd("colorscheme vscode")
 
-vim.lsp.enable({ "lua_ls"})
+vim.lsp.enable({ "lua_ls", "lemminx" })

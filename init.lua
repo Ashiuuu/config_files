d36f3cbe144abd74ef9c085d601a45fc8f4f8a35
Ashiuuu <ushiuako@gmail.com>
2025-08-29 23:12:06 +0200

    git commit test

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.o.winborder = "rounded"
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
@@ -11,14 +12,22 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>pu', vim.pack.update)
-vim.keymap.set('n', '<leader>gs', function()
-	vim.cmd('Git status')
-end)
+
+-- Git fugitive shortcuts
+vim.keymap.set('n', '<leader>gg', ':Git<CR>')
+vim.keymap.set('n', '<leader>gs', ':Git status<CR>')
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 vim.keymap.set('n', '<leader>gc', function()
-	vim.cmd('Git commit')
-end)
-vim.keymap.set('n', '<leader>gp', function()
-	vim.cmd('Git push')
+	local buf = vim.api.nvim_create_buf(true, true)
+	vim.api.nvim_create_autocmd('BufLeave', { buffer = buf, callback = function(ev)
+		local msg = table.concat(vim.api.nvim_buf_get_lines(ev.buf, 0, vim.api.nvim_buf_line_count(0), false), "\n")
+		vim.api.nvim_echo({{string.format('dir: %s', vim.fn.getcwd())}}, false, {})
+		vim.cmd(string.format("Git commit -m '%s'", msg))
+	end})
+	local opts = {relative='cursor', width=30, height=1, col=0, row=1, anchor='NW', style='minimal', border={"╔", "═" ,"╗", "║", "╝", "═", "╚", "║"}, title="Commit Message", title_pos="center"}
+	local win = vim.api.nvim_open_win(buf, 0, opts)
+	 
+	--vim.cmd('Git commit')
 end)
 
 vim.pack.add({

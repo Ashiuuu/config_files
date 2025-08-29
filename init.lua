e3078fc923985409b83cf25d7591b5e23c4ae1ce
Ashiuuu <ushiuako@gmail.com>
2025-08-29 11:10:30 +0200

    add git support

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
 vim.o.wrap = false
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.swapfile = false
 vim.o.clipboard = "unnamedplus"
 
@@ -10,11 +11,15 @@ vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>pu', vim.pack.update)
+vim.keymap.set('n', '<leader>gs', function()
+	vim.cmd('Git status')
+end)
 
 vim.pack.add({
 	"https://github.com/Mofiqul/vscode.nvim",
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/tpope/vim-fugitive",
 })
 
 require("oil").setup()

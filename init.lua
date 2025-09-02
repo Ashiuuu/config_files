a8809dc2f439ad4724588a44b7923acce4e1632e
Ashiuuu <ushiuako@gmail.com>
2025-09-03 01:15:44 +0200

    add lsp autocomplete

--- init.lua
+++ init.lua
@@ -92,6 +92,17 @@ vim.cmd("colorscheme vscode")
 vim.lsp.enable({ "lua_ls", "lemminx", "rust_analyzer" })
 vim.lsp.inlay_hint.enable()
 
+-- lsp autocomplete
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
 -- fix the diagnostics for vim global variable
 vim.lsp.config("lua_ls", {
 	settings = {

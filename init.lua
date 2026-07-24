e3985a5f6305d02f8d6389ebc50c843248195fa1
CMI <clement.milisavljevic@devoteam.com>
2026-07-24 14:59:18 +0200

    QoL on paste

--- init.lua
+++ init.lua
@@ -77,7 +77,7 @@ require("image").setup()
 -- ============================
 -- Plugin keymaps
 -- Img-clio
-vim.keymap.set('n', '<leader>pip', ':PasteImage<CR>')
+vim.keymap.set('n', '<leader>pip', ':PasteImage<CR><CR>')
 
 -- Oil
 vim.keymap.set('n', '<leader>oo', ':Oil<CR>')

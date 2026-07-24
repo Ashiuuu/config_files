f8460639179b952be38b6a9ec75bf76d6f3a77ae
CMI <clement.milisavljevic@devoteam.com>
2026-07-24 15:25:19 +0200

    switch to fancy

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = false
 
 -- Color scheme / font
@@ -54,10 +54,13 @@ config.mouse_bindings = {
   },
 }
 
+config.window_frame = {
+	font_size = 12
+}
+
 --------------------------------------------------------------------------------
 -- 1. TERMINATOR TAB BAR FRAME & CONTAINER
 --------------------------------------------------------------------------------
-config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = false
 config.show_tab_index_in_tab_bar = false
@@ -137,10 +140,5 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
     }
   end
 end)
-
-config.window_frame = {
-  font_size = 34.0, -- Adjust this number to make the tabs as big as you want
-}
-
 
 return config
